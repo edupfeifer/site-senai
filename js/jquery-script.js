981dd07c209529133cdd0b0ad186a43a464d6057
edupfeifer <135552450+edupfeifer@users.noreply.github.com>
2023-07-03 18:50:53 -0300

    primeiro commit

---
+++ js/jquery-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  
+  $("#botao-cadastrar").click(function(){
+    $("#form-cadastrar").slideToggle("slow");
+    $("#section-login").slideToggle("slow");
+    $("#botao-cadastrar").hide();
+  })
+})
+
+//efeito de esconder formulário de cadastro
